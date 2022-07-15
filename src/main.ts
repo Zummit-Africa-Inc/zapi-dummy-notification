a21cb4d7336f339578aca1102bd3a51065b572b7
Brain Philemon <philbrainy@gmail.com>
2022-07-15 08:04:19 +0100

    necessary installs and node port

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options)
     SwaggerModule.setup('api-notify', app, document)
   }
-  await app.listen(3000);
+  await app.listen(Number(process.env.NODE_PORT) || 3111);
 }
 bootstrap();
